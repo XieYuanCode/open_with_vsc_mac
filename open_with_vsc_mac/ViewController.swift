e878e929d6aed5520861e24c0acbe4125d312f80
谢渊 <xieyuan@agree.com.cn>
2021-11-16 14:55:18 +0800

    Initial Commit

---
+++ open_with_vsc_mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  open_with_vsc_mac
+//
+//  Created by 谢渊 on 2021/11/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
